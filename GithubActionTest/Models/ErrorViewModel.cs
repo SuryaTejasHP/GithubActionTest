e053cdaa0da0aa3be598f9726ba51c39b82d1e3b
H P, Surya Tejas SBOBNG-PTIY EM NS <Surya-Tejas.HP@shell.com>
2026-01-06 20:35:57 +0530

    Add project files.

---
+++ GithubActionTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GithubActionTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
